09bcf46060192780ce2f815f74cfd7d0597ab791
nanpuyue <nanpuyue@gmail.com>
2019-05-08 17:17:07 +0800

    add `print_dot()`

--- src/util.rs
+++ src/util.rs
@@ -1,26 +1,44 @@
+use core::fmt::Display;
+
 use super::*;
 
 pub fn validate<T: PartialOrd>(tree: &AvlTreeNode<T>) -> bool {
-    match tree {
-        None => {}
-        Some(root) => {
-            if root.height != max(root.left.height(), root.right.height()) + 1 {
+    if let Some(root) = tree {
+        if root.height != max(root.left.height(), root.right.height()) + 1 {
+            return false;
+        }
+        if tree.balance_factor().abs() > 1 {
+            return false;
+        }
+        if let Some(x) = &root.left {
+            if !(x.val < root.val && validate(&root.left)) {
                 return false;
             }
-            if tree.balance_factor().abs() > 1 {
+        }
+        if let Some(x) = &root.right {
+            if !(x.val > root.val && validate(&root.right)) {
                 return false;
-            }
-            if let Some(x) = &root.left {
-                if !(x.val < root.val && validate(&root.left)) {
-                    return false;
-                }
-            }
-            if let Some(x) = &root.right {
-                if !(x.val > root.val && validate(&root.right)) {
-                    return false;
-                }
             }
         }
     }
     true
+}
+
+pub fn print_dot<T: PartialOrd + Display>(tree: &AvlTreeNode<T>, parent: &AvlTreeNode<T>) {
+    if parent.is_none() {
+        println!("digraph {{");
+    }
+    if let Some(root) = tree {
+        if let Some(x) = &root.left {
+            println!("    {}->{}", root.val, x.val);
+            print_dot(&root.left, tree);
+        }
+        if let Some(x) = &root.right {
+            println!("    {}->{}", root.val, x.val);
+            print_dot(&root.right, tree);
+        }
+    }
+    if parent.is_none() {
+        println!("}}");
+    }
 }
